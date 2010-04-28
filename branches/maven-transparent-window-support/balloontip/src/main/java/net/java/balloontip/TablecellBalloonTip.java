cb5bdf5e76c89ddd431ed389d94836cc8872c122
thierryblind <thierryblind@23820abc-fd69-4647-b2b3-9d2b0b2f73e9>
2010-04-28 15:05:53 +0000

    TablecellBalloonTip : minor changes

--- branches/maven-transparent-window-support/balloontip/src/main/java/net/java/balloontip/TablecellBalloonTip.java
+++ branches/maven-transparent-window-support/balloontip/src/main/java/net/java/balloontip/TablecellBalloonTip.java
@@ -121,9 +121,9 @@ public class TablecellBalloonTip extends CustomBalloonTip {
 	}
 
 	public void closeBalloon() {
-		super.closeBalloon();
 		((JTable)attachedComponent).getColumnModel().removeColumnModelListener(columnListener);
 		attachedComponent.removeAncestorListener(attachedComponentParentListener);
+		super.closeBalloon();
 	}
 
 	/*
