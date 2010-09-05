b2dd39c960d45271dfc47787e7b1faa10301a38e
Tim Molderez <timmolderez@users.noreply.github.com>
2010-09-05 01:37:02 +0000

    Switching to LGPL v2.1 (copyright notice changed in every source file)

--- project/balloontip-examples/src/main/java/net/java/balloontip/examples/positioner/SimplePositionerExample.java
+++ project/balloontip-examples/src/main/java/net/java/balloontip/examples/positioner/SimplePositionerExample.java
@@ -1,21 +1,21 @@
 /**
  * Balloontip - Balloon tips for Java Swing applications
- * Copyright 2007, 2008 Bernhard Pauler, Tim Molderez
+ * Copyright 2007-2010 Bernhard Pauler, Tim Molderez
  * 
  * This file is part of Balloontip.
  * 
- * Balloontip is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Balloontip is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * Balloontip is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Balloontip.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Balloontip. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package net.java.balloontip.examples.positioner;
@@ -31,6 +31,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import net.java.balloontip.BalloonTip;
 import net.java.balloontip.styles.EdgedBalloonStyle;
 
