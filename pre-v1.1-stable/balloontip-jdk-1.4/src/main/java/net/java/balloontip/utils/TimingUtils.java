e267143094e6ea286f42907838a5074ad3d66e87
thierryblind <thierryblind@23820abc-fd69-4647-b2b3-9d2b0b2f73e9>
2010-10-25 12:23:12 +0000

    Pre-1.1 version of the balloontip project, before integration of latest Tim's commits. This version is compatible with java 1.4

---
+++ pre-v1.1-stable/balloontip-jdk-1.4/src/main/java/net/java/balloontip/utils/TimingUtils.java
@@ -0,0 +1,68 @@
+/**
+ * Balloontip - Balloon tips for Java Swing applications
+ * Copyright 2007, 2008 Bernhard Pauler, Tim Molderez
+ *
+ * This file is part of Balloontip.
+ *
+ * Balloontip is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Balloontip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Balloontip.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.java.balloontip.utils;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+import net.java.balloontip.BalloonTip;
+
+/**
+ * This class provides timed balloon tips
+ * @author Tim
+ */
+public class TimingUtils {
+
+	/*
+	 * Disallow instantiating this class
+	 */
+	private TimingUtils() {}
+
+	/**
+	 * Displays a balloon tip for a certain time.
+	 * @param balloon			the BalloonTip
+	 * @param time				show the balloon for this amount of milliseconds
+	 */
+	// This method was created to have "compiler compliancy" with all versions of java
+	// and with all existing versions of the balloon tip library.
+	public static void showTimedBalloon(final BalloonTip balloon, int time) {
+		showTimedBalloon(balloon, new Integer(time));
+	}
+
+	/**
+	 * Displays a balloon tip for a certain time.
+	 * @param balloon			the BalloonTip
+	 * @param time				show the balloon for this amount of milliseconds
+	 */
+	public static void showTimedBalloon(final BalloonTip balloon, Integer time) {
+		balloon.setVisible(true);
+		Timer timer = new Timer(0, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				balloon.closeBalloon();
+			}
+		});
+		timer.setRepeats(false);
+		timer.setInitialDelay(time.intValue());
+		timer.start();
+	}
+}
