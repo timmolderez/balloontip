f373ab06cb8c1e835cb4f0f76acbcec3d675f9f9
nferno <nferno@23820abc-fd69-4647-b2b3-9d2b0b2f73e9>
2013-10-05 10:39:08 +0000

    [maven-release-plugin]  copy for tag balloontip-parent-1.2.4

---
+++ tags/balloontip-parent-1.2.4/balloontip-examples/src/main/java/net/java/balloontip/examples/complete/panels/BehaviourTab.java
@@ -0,0 +1,224 @@
+/**
+ * Copyright (c) 2011-2013 Bernhard Pauler, Tim Molderez.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the 3-Clause BSD License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/BSD-3-Clause
+ */
+
+package net.java.balloontip.examples.complete.panels;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+import javax.swing.table.TableColumn;
+
+import net.java.balloontip.TableCellBalloonTip;
+import net.java.balloontip.examples.complete.Utils;
+import net.java.balloontip.positioners.BasicBalloonTipPositioner;
+import net.java.balloontip.positioners.LeftAbovePositioner;
+import net.java.balloontip.positioners.LeftBelowPositioner;
+import net.java.balloontip.positioners.RightAbovePositioner;
+import net.java.balloontip.positioners.RightBelowPositioner;
+
+/**
+ * Behaviour tab of the demo application; demonstrates different balloon tip positioners
+ * @author Tim Molderez
+ */
+public class BehaviourTab extends JPanel {
+	private final JComboBox<?> alignmentPicker;
+	private final JComboBox<?> attachPicker;
+	private final JCheckBox offsetCorrection;
+	private final JCheckBox orientationCorrection;
+	private final TableCellBalloonTip tableBalloon;
+
+	private final static int HOFFSET = 40;
+	private final static int VOFFSET = 20;
+
+	public BehaviourTab() {
+		super();
+		setLayout(new GridBagLayout());
+		int gridY = 0;
+
+		/*
+		 * Draw the GUI
+		 */
+
+		// Description label
+		add(new JLabel("<html>Scroll around the table and notice the balloon tip's behaviour when it collides with the window's border. You can alter this behaviour using the settings below.</html>"), new GridBagConstraints(0,gridY,2,1,1.0,0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(10,10,25,0), 0, 0));
+		++gridY;
+
+		// Orientation combobox
+		add(new JLabel("Orientation:"), new GridBagConstraints(0,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,10,0,0), 0, 0));
+		String[] alignmentOptions = {"Left, above component", "Left, below component", "Right, above component", "Right, below component"};
+		alignmentPicker = new JComboBox<Object>(alignmentOptions);
+		add(alignmentPicker, new GridBagConstraints(1,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2,10,2,0), 0, 0));
+		++gridY;
+
+		// Attaching location combobox
+		add(new JLabel("Attaching location:"), new GridBagConstraints(0,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,10,0,0), 0, 0));
+		String[] attachOptions = {"Aligned", "Center", "North", "North east", "East", "South east", "South", "South west", "West", "North west"};
+		attachPicker = new JComboBox<Object>(attachOptions);
+		add(attachPicker, new GridBagConstraints(1,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2,10,2,0), 0, 0));
+		++gridY;
+
+		// Offset correction checkbox
+		add(new JLabel("Offset correction:"), new GridBagConstraints(0,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,10,0,0), 0, 0));
+		offsetCorrection = new JCheckBox();
+		offsetCorrection.setSelected(true);
+		Utils.setToolTip(offsetCorrection, "<html>Offset correction will adjust the horizontal offset<br>when the balloon tip collides with the window border</html>");
+		add(offsetCorrection, new GridBagConstraints(1,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2,10,2,0), 0, 0));
+		++gridY;
+
+		// Orientation correction checkbox
+		add(new JLabel("Orientation correction:"), new GridBagConstraints(0,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,10,0,0), 0, 0));
+		orientationCorrection = new JCheckBox();
+		orientationCorrection.setSelected(true);
+		Utils.setToolTip(orientationCorrection, "<html>Orientation correction will flip the balloon tip<br>when it collides with the window border</html>");
+		add(orientationCorrection, new GridBagConstraints(1,gridY,1,1,0.0,0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2,10,2,0), 0, 0));
+		++gridY;
+
+		// Table
+		final JTable table = new JTable(64,32);
+		table.getModel().setValueAt("Cell with a balloon tip", 32, 16);
+		table.setTableHeader(null); // Remove the table header
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		TableColumn column = null;
+		for (int i=0; i<32; ++i) {
+			column = table.getColumnModel().getColumn(i);
+			column.setPreferredWidth(120);
+		}
+		final JScrollPane tableScrollPane = new JScrollPane(table);
+		tableScrollPane.getHorizontalScrollBar().setUnitIncrement(3);
+		tableScrollPane.getVerticalScrollBar().setUnitIncrement(3);
+		add(tableScrollPane, new GridBagConstraints(0,gridY,2,1,1.0,1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(10,10,10,10), 0, 0));
+
+		/*
+		 * Add the GUI's behaviour
+		 */
+
+		// Tablecell balloon tip
+		tableBalloon = new TableCellBalloonTip(table, 
+				new JLabel("Use the scrollbars to move me around."), 32, 16,
+				Utils.createBalloonTipStyle(), 
+				new LeftAbovePositioner(HOFFSET, VOFFSET),
+				null);
+		
+		table.addAncestorListener(new AncestorListener() {
+			public void ancestorAdded(AncestorEvent event) {
+				table.scrollRectToVisible(table.getCellRect(38, 18, true));
+			}
+			public void ancestorMoved(AncestorEvent event) {}
+			public void ancestorRemoved(AncestorEvent event) {}
+		});
+
+		// Alignment combo box
+		alignmentPicker.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BasicBalloonTipPositioner positioner = null;
+
+				switch(alignmentPicker.getSelectedIndex()) {
+				case 0:
+					positioner = new LeftAbovePositioner(HOFFSET, VOFFSET);
+					break;
+				case 1:
+					positioner = new LeftBelowPositioner(HOFFSET, VOFFSET);
+					break;
+				case 2:
+					positioner = new RightAbovePositioner(HOFFSET, VOFFSET);
+					break;
+				case 3:
+					positioner = new RightBelowPositioner(HOFFSET, VOFFSET);
+					break;
+				}
+
+				copyPositionerSettings(((BasicBalloonTipPositioner)tableBalloon.getPositioner()), positioner);
+				tableBalloon.setPositioner(positioner);
+			}
+		});
+
+		// Attach location combo box
+		attachPicker.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((BasicBalloonTipPositioner)tableBalloon.getPositioner()).enableFixedAttachLocation(true);
+
+				switch(attachPicker.getSelectedIndex()) {
+				case 0:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).enableFixedAttachLocation(false);
+					break;
+				case 1:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(0.5f, 0.5f);
+					break;
+				case 2:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(0.5f, 0.0f);
+					break;
+				case 3:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(1.0f, 0.0f);
+					break;
+				case 4:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(1.0f, 0.5f);
+					break;
+				case 5:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(1.0f, 1.0f);
+					break;
+				case 6:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(0.5f, 1.0f);
+					break;
+				case 7:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(0.0f, 1.0f);
+					break;
+				case 8:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(0.0f, 0.5f);
+					break;
+				case 9:
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).setAttachLocation(0.0f, 0.0f);
+					break;
+				}
+				tableBalloon.refreshLocation();
+			}
+		});
+
+		// Offset correction
+		offsetCorrection.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (offsetCorrection.isSelected()) {
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).enableOffsetCorrection(true);
+				} else {
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).enableOffsetCorrection(false);
+				}
+				tableBalloon.refreshLocation();
+			}
+		});
+
+		// Orientation correction
+		orientationCorrection.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (orientationCorrection.isSelected()) {
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).enableOrientationCorrection(true);
+				} else {
+					((BasicBalloonTipPositioner)tableBalloon.getPositioner()).enableOrientationCorrection(false);
+				}
+				tableBalloon.refreshLocation();
+			}
+		});
+	}
+
+	private void copyPositionerSettings(final BasicBalloonTipPositioner oldPositioner, BasicBalloonTipPositioner newPositioner) {
+		newPositioner.enableOffsetCorrection(oldPositioner.isOffsetCorrected());
+		newPositioner.enableOrientationCorrection(oldPositioner.isOrientationCorrected());
+		newPositioner.enableFixedAttachLocation(oldPositioner.isFixedAttachLocation());
+		newPositioner.setAttachLocation(oldPositioner.getAttachLocationX(), oldPositioner.getAttachLocationY());
+	}
+}
---
+++ tags/balloontip-parent-1.2.4/balloontip-examples/src/main/java/net/java/balloontip/examples/complete/panels/ContentsTab.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2011-2013 Bernhard Pauler, Tim Molderez.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the 3-Clause BSD License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/BSD-3-Clause
+ */
+
+package net.java.balloontip.examples.complete.panels;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+
+import net.java.balloontip.BalloonTip;
+import net.java.balloontip.TableCellBalloonTip;
+import net.java.balloontip.examples.complete.CompleteExample;
+import net.java.balloontip.examples.complete.Utils;
+
+/**
+ * Contents tab of the demo application; demonstrates that a balloon tip can contain all sorts of components
+ * @author Tim Molderez
+ */
+public class ContentsTab extends JPanel {
+	/**
+	 * Default constructor
+	 */
+	public ContentsTab() {
+		super();
+		setLayout(new GridBagLayout());
+		int gridY = 0;
+
+		/*
+		 * Draw the GUI
+		 */
+
+		// Description label
+		add(new JLabel("<html>A balloon tip can contain many types of " + Utils.monospace("JComponent") + " beside just text.</html>"), new GridBagConstraints(0,gridY,2,1,1.0,0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(10,10,25,0), 0, 0));
+		++gridY;
+
+		// Balloon tip 1
+		JLabel label1 = new JLabel("A balloon tip with an icon");
+		add(label1, new GridBagConstraints(0,gridY,1,1,1.0,1.0, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0,0,80,0), 0, 0));
+		
+		JLabel contents1= new JLabel("I've got an icon!");
+		contents1.setIcon(new ImageIcon(CompleteExample.class.getResource("/net/java/balloontip/images/frameIcon.png")));
+		contents1.setIconTextGap(10);
+		new BalloonTip(label1, contents1,
+				Utils.createBalloonTipStyle(),
+				Utils.createBalloonTipPositioner(), 
+				null);
+		
+		// Balloon tip 2
+		JLabel label2 = new JLabel("A balloon tip with tabs");
+		add(label2, new GridBagConstraints(1,gridY,1,1,1.0,1.0, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0,0,80,0), 0, 0));
+		++gridY;
+		
+		JTabbedPane contents2 = new JTabbedPane();
+		JPanel tab1 = new JPanel();
+		tab1.add(new JCheckBox());
+		tab1.add(new JLabel("Tabs? But of course!"));
+		JPanel tab2 = new JPanel();
+		tab2.add(new JLabel("Because we can!"));
+		contents2.addTab("FirstTab", tab1);
+		contents2.addTab("SecondTab", tab2);
+		new BalloonTip(label2, contents2,
+				Utils.createBalloonTipStyle(),
+				Utils.createBalloonTipPositioner(), 
+				null);
+
+		// Balloon tip 3
+		JLabel label3 = new JLabel("A balloon tip a table");
+		add(label3, new GridBagConstraints(0,gridY,1,1,1.0,1.0, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0,0,80,0), 0, 0));
+		
+		JTable table = new JTable(8,2);
+		table.setTableHeader(null);
+		table.getModel().setValueAt("Balloon tip", 0, 0);
+		table.getModel().setValueAt("with", 1, 1);
+		table.getModel().setValueAt("a", 2, 0);
+		table.getModel().setValueAt("table", 3, 1);
+		JScrollPane tableScrollPane = new JScrollPane(table);
+		tableScrollPane.setPreferredSize(new Dimension(150,100));
+		tableScrollPane.setBackground(Color.WHITE);
+		new BalloonTip(label3, tableScrollPane,
+				Utils.createBalloonTipStyle(),
+				Utils.createBalloonTipPositioner(), 
+				null);
+		
+		// Balloon tip 4
+		JLabel label4 = new JLabel("Nesting balloon tips");
+		add(label4, new GridBagConstraints(1,gridY,1,1,1.0,1.0, GridBagConstraints.SOUTH, GridBagConstraints.NONE, new Insets(0,0,80,0), 0, 0));
+		++gridY;
+		JTable comp1 = new JTable(8,2);
+		comp1.setTableHeader(null);
+		comp1.getModel().setValueAt("This", 0, 0);
+		comp1.getModel().setValueAt("just", 1, 1);
+		comp1.getModel().setValueAt("might", 2, 0);
+		JScrollPane panel1 = new JScrollPane(comp1);
+		panel1.setPreferredSize(new Dimension(100, 75));
+		panel1.setBackground(Color.WHITE);
+		new BalloonTip(label4, panel1,
+				Utils.createBalloonTipStyle(),
+				Utils.createBalloonTipPositioner(), 
+				null);
+		
+		JPanel panel2 = new JPanel();
+		panel2.setBackground(Color.WHITE);
+		final JLabel comp2 = new JLabel("be a bit");
+		comp2.setIcon(new ImageIcon(CompleteExample.class.getResource("/net/java/balloontip/images/frameIcon.png")));
+		panel2.add(comp2);
+		final TableCellBalloonTip tableBt = new TableCellBalloonTip(comp1, panel2, 1,1,
+				Utils.createBalloonTipStyle(),
+				Utils.createBalloonTipPositioner(), 
+				null);
+		tableBt.setPadding(0);
+		
+		JTabbedPane panel3 = new JTabbedPane();
+		JPanel panel3Tab1 = new JPanel();
+		panel3Tab1.add(new JLabel("overkill?"));
+		JPanel panel3Tab2 = new JPanel();
+		panel3Tab2.add(new JLabel("Don't you think?"));
+		panel3.addTab("A", panel3Tab1);
+		panel3.addTab("B", panel3Tab2);
+		new BalloonTip(comp2, panel3,
+				Utils.createBalloonTipStyle(),
+				Utils.createBalloonTipPositioner(), 
+				null);
+	}
+}
