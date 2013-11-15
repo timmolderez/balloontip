7af664f960e8376fda76a40ff8cd51047e0aa579
Tim Molderez <timmolderez@users.noreply.github.com>
2013-11-15 01:48:37 +0000

    BALLOONTIP-10 fixed; removed workaround for this bug in CustomBalloonTip.java. Ready for release 1.2.4.1.

--- balloontip/src/main/java/net/java/balloontip/utils/ToolTipUtils.java
+++ balloontip/src/main/java/net/java/balloontip/utils/ToolTipUtils.java
@@ -72,6 +72,7 @@ public final class ToolTipUtils {
 		
 		public void mouseMoved(MouseEvent e) {
 			if(balloonTip instanceof CustomBalloonTip) {
+				// If the mouse is within the balloon tip's attached rectangle
 				if (((CustomBalloonTip)balloonTip).getOffset().contains(e.getPoint())) {
 					if (!balloonTip.isVisible() && !initialTimer.isRunning()) {
 						initialTimer.start();
@@ -81,17 +82,6 @@ public final class ToolTipUtils {
 					balloonTip.setVisible(false);
 				}
 			}
-			
-			// If the mouse is within the balloon tip's attached rectangle
-			//TODO Fix bug: e.getpoint gets coordinates relative to component! getAttachedRect check should only be for specialized uses, i.e. when attaching to a list
-//			if (balloonTip.getAttachedRectangle().contains(e.getPoint())) {
-//				if (!balloonTip.isVisible() && !initialTimer.isRunning()) {
-//					initialTimer.start();
-//				}
-//			} else {
-//				stopTimers();
-//				balloonTip.setVisible(false);
-//			}
 		}
 
 		public void mouseExited(MouseEvent e) {
